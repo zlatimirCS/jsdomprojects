6429e1bf40e65b27e3379d327c23772e7599d913
zlatimirCS <zlatimir_rk@yahoo.com>
2023-05-23 18:57:05 +0200

    additional tasks added

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,30 +1,46 @@
 //using selectors inside the element
 // traversing the dom
 
-const buttons = document.querySelectorAll('.btn');
-const content = document.querySelector('.content');
-const containerChildren = Array.from(
-  document.querySelector('.container').children
-);
+// const buttons = document.querySelectorAll('.btn');
+// const content = document.querySelector('.content');
+// const containerChildren = Array.from(
+//   document.querySelector('.container').children
+// );
 
-buttons.forEach((btn) => {
-  btn.addEventListener('mouseenter', (e) => {
-    console.log('mouse enter', e.currentTarget);
-    e.currentTarget.classList.add('hover');
-  });
-  btn.addEventListener('mouseleave', (e) => {
-    console.log('mouse leave', e.currentTarget);
-    e.currentTarget.classList.remove('hover');
-  });
-  btn.addEventListener('click', (e) => {
-    containerChildren.forEach((child) => {
-      if (child.classList.contains('show-text')) {
-        child.classList.remove('show-text');
-        child.querySelector('.btn').innerHTML = '<i class="fas fa-plus"></i>';
+// buttons.forEach((btn) => {
+//   btn.addEventListener('mouseenter', (e) => {
+//     console.log('mouse enter', e.currentTarget);
+//     e.currentTarget.classList.add('hover');
+//   });
+//   btn.addEventListener('mouseleave', (e) => {
+//     console.log('mouse leave', e.currentTarget);
+//     e.currentTarget.classList.remove('hover');
+//   });
+//   btn.addEventListener('click', (e) => {
+//     containerChildren.forEach((child) => {
+//       if (child.classList.contains('show-text')) {
+//         child.classList.remove('show-text');
+//         child.querySelector('.btn').innerHTML = '<i class="fas fa-plus"></i>';
+//       }
+//     });
+//     const question = e.currentTarget.parentElement.parentElement;
+//     question.classList.toggle('show-text');
+//     btn.innerHTML = '<i class="fas fa-minus"></i>';
+//   });
+// });
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+  const btn = question.querySelector('.btn');
+  btn.addEventListener('click', () => {
+    questions.forEach((item) => {
+      if (item !== question) {
+        item.classList.remove('show-text');
+        // item.querySelector('.btn').innerHTML = '<i class="fas fa-plus"></i>';
       }
     });
-    const question = e.currentTarget.parentElement.parentElement;
     question.classList.toggle('show-text');
-    btn.innerHTML = '<i class="fas fa-minus"></i>';
+    // btn.innerHTML = '<i class="fas fa-minus"></i>';
   });
 });
